567d639e2e5d481f6e34fa84ff6ceaef6809a344
Michael Sotir <sotirm@umich.edu>
2020-05-01 15:40:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,6 @@ def find_even_values(src)
   
 count = 0
 while count < src.length do 
-  count = count + 1
   inner_count = 0
   while inner_count < src[count].length do 
     if src[count][inner_count] % 2 == 0
